b894cbef8804c851141de382514fad15fc112d44
Ajay Kumar BG <ajaykumar.bg@perfomatix.com>
2019-05-15 11:39:17 +0530

    Changed employee item and skill item class components to functional components

--- src/components/manage-employees/EmployeeItem.js
+++ src/components/manage-employees/EmployeeItem.js
@@ -1,30 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 
-class EmployeeItem extends Component {
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-         item: props.item
-      }
-    }
-    
-  render() {
-    const { id, employeeName, employeeId, designation, emailId} = this.state.item;
-    return (
-        <li className="collection-item avatar">
-          <Link to={`/employees/${id}`}>
-            <i className="fa fa-user circle"></i>
-            <span className="title">{ employeeName } : {employeeId}</span>
-            <p>{ designation } <br/>
-              { emailId }
-            </p>
-          </Link>
-          <Link to={`/employees/edit/${id}`} className="secondary-content"><i className="fa fa-pencil"></i></Link>
-        </li>
-    )
-  }
+function EmployeeItem(props) {
+  const { id, employeeName, employeeId, designation, emailId} = props.item;
+  return (
+    <li className="collection-item avatar">
+      <Link to={`/employees/${id}`}>
+        <i className="fa fa-user circle"></i>
+        <span className="title">{ employeeName } : {employeeId}</span>
+        <p>{ designation } <br/>
+          { emailId }
+        </p>
+      </Link>
+      <Link to={`/employees/edit/${id}`} className="secondary-content"><i className="fa fa-pencil"></i></Link>
+    </li>
+  )
 }
 
 export default EmployeeItem
--- src/components/manage-skills/SkillItem.js
+++ src/components/manage-skills/SkillItem.js
@@ -1,25 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 
-class SkillItem extends Component {
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-         item: props.item
-      }
-    }
-    
-  render() {
-    const { id, name, version} = this.state.item;
-    return (
-        <li className="collection-item">
-          <Link to={`/skills/${id}`}>
-              { name } : {version}
-          </Link>
-        </li>
-    )
-  }
+function SkillItem(props) {
+  const { id, name, version} = props.item;
+  return (
+    <li className="collection-item">
+      <Link to={`/skills/${id}`}>
+        { name } : {version}
+      </Link>
+    </li>
+  )
 }
 
 export default SkillItem
